276f48f94862f4dbbeff04fe7a5f6f4fba099d6c
Aamer Khan <105784601+aamerkhan07@users.noreply.github.com>
2023-08-22 20:22:53 +0530

    Create variable.tf

---
+++ variable.tf
@@ -0,0 +1,15 @@
+variable "filename" {
+default = "new file.txt"
+}
+variable "content" {
+default = "aamer khan"
+  }
+variable "prefix" {
+default = "mr"
+}
+variable "separator" {
+default = ":"
+}
+variable "length" {
+default = "1"
+}
